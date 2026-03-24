c5170ec8f5b8e57a191ad8b37118856fc2eeb500
hugo.smitter@gmail.com <hugo.smitter@gmail.com>
2026-03-24 03:03:51 +0000

    Created Input_Display_Components_Form; Edited Layout_Components_Form

---
+++ client_code/Input_Display_Components_Form/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Input_Display_Components_FormTemplate
+from anvil import *
+import plotly.graph_objects as go
+
+class Input_Display_Components_Form(Input_Display_Components_FormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  @handle("link_2", "click")
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Layout_Components_Form')
+    pass
--- client_code/Layout_Components_Form/__init__.py
+++ client_code/Layout_Components_Form/__init__.py
@@ -7,3 +7,9 @@ class Layout_Components_Form(Layout_Components_FormTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("link_1", "click")
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Input_Display_Components_Form')
+    pass
